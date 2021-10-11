fcdd8d983126c98cf6e1742bf517244b668723b7
Osama Yousafzai <osama.yousafzai@gmail.com>
2021-10-11 21:37:04 +0300

    Updates

---
+++ precache-manifest.2a769fba6fdfe8ffa4d58de66dc2f0cd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1ac7ef9875eddb0c2e07",
+    "url": "/counter-simple/static/css/main.655e3700.chunk.css"
+  },
+  {
+    "revision": "1ac7ef9875eddb0c2e07",
+    "url": "/counter-simple/static/js/main.1ac7ef98.chunk.js"
+  },
+  {
+    "revision": "eb8fd53bee21e4f0f17b",
+    "url": "/counter-simple/static/js/1.eb8fd53b.chunk.js"
+  },
+  {
+    "revision": "b6ab62352bf6f88d428b",
+    "url": "/counter-simple/static/js/runtime~main.b6ab6235.js"
+  },
+  {
+    "revision": "9cdeeb24860f689696f0ae33679c3d83",
+    "url": "/counter-simple/index.html"
+  }
+];
